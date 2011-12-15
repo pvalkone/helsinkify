99796b6f784a3ac0d02883ec91d121191ac9825f
Petteri Valkonen <petteri.valkonen@iki.fi>
2011-12-15 21:24:26 +0200

    Include description and license information in the script itself.

--- helsinkify.user.js
+++ helsinkify.user.js
@@ -1,3 +1,14 @@
+// Helsinkify - A Greasemonkey user script for adding Spotify links to Radio
+// Helsinki's playlist pages. 
+//
+// Copyright Petteri Valkonen <petteri.valkonen@iki.fi>, 2011
+//
+// This program is free software. It comes without any warranty, to the extent
+// permitted by applicable law. You can redistribute it and/or modify it under
+// the terms of the Do What The Fuck You Want To Public License, Version 2, as
+// published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more
+// details.
+ 
 // ==UserScript==
 // @name        Helsinkify
 // @namespace   https://github/pvalkone
